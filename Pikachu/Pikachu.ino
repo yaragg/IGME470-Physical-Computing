b3b31c966b43195e6e1be61a0ceed7e368f50fd9
Yara Grassi Gouffon (RIT Student) <yxg5358@ad.rit.edu>
2016-02-17 09:39:59 -0500

    Trying to figure out the buzzer with millis, step by step. This should make it keep alternating between sounds but it's not working. It just keeps beeping the same sound over and over quickly.

--- Pikachu/Pikachu.ino
+++ Pikachu/Pikachu.ino
@@ -17,6 +17,7 @@ int tones[] = {494, 247};
 float beats[] = {0.5, 1.5};
 int tones2[] = {494, 247, 494};
 float beats2[] = {0.5, 0.7, 0.5};
+int soundIndex = 0;
 
 unsigned long previousSound = 0;
 const long soundInterval = 2000;
@@ -43,19 +44,25 @@ void loop() {
   analogWrite(cheek2, cheekValue);
   analogWrite(flash1, flashValue);
   analogWrite(flash2, flashValue);
+  
+  if(millis() - previousSound > beats[soundIndex]){
+    soundIndex = (soundIndex + 1)%2;
+    tone(speakerPin, tones[soundIndex], beats[soundIndex]);
+    previousSound = millis();
+  }
 
- /* unsigned long current = millis();
+  /*unsigned long current = millis();
   if(playing || fsrValue > 20 && current - previousSound >= soundInterval){
     previousSound = current;
-    for(int i=0; i<2; i++){
-      if(current < previousSound + (unsigned long)beats[i]*500){
+    //for(int i=0; i<2; i++){
+      if(current < previousSound + (unsigned long)beats[soundIndex]*500){
         if(!playing){
-          tone(speakerPin, tones[i]);  
+          tone(speakerPin, tones[soundIndex]);  
           playing = true;
         }
       }
-      else playing = false;   
-    }
+      else { playing = false;   soundIndex = (soundIndex+1)%2;}
+    //}
   }
   else if(!playing) noTone(speakerPin);
 
