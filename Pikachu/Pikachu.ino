8b2199f573fa29a18372dd407db83f048912f879
Yara Grassi Gouffon (RIT Student) <yxg5358@ad.rit.edu>
2016-02-17 09:42:22 -0500

    Cleaned up code and removed buzzer part that was not in use.

--- Pikachu/Pikachu.ino
+++ Pikachu/Pikachu.ino
@@ -4,25 +4,11 @@ const int flash1 = 3; //Yellow LED for "flash" move
 const int flash2 = 9; //Other yellow LED for the "flash" move
 const int fsr = A0; //FSR in the forehead
 const int light = A1; //Light sensor
-const int speakerPin = 12; //pin for the buzzer
 
 int fsrValue = 0; //Value read from the FSR
 int cheekValue = 0; //Output to the cheeks
 int lightValue = 0; //Light sensor reading
 int flashValue = 0; //Output to "flash" LEDs
-
-//Notes and beats for the two cries
-int tones[] = {494, 247};
-//float beats[] = {0.5, 1};
-float beats[] = {0.5, 1.5};
-int tones2[] = {494, 247, 494};
-float beats2[] = {0.5, 0.7, 0.5};
-
-unsigned long previousSound = 0;
-const long soundInterval = 2000;
-const long duration1 = 800;
-const long duration2 = 900;
-bool playing = false;
 
 const int debug = 1;
 
@@ -44,23 +30,6 @@ void loop() {
   analogWrite(flash1, flashValue);
   analogWrite(flash2, flashValue);
 
- /* unsigned long current = millis();
-  if(playing || fsrValue > 20 && current - previousSound >= soundInterval){
-    previousSound = current;
-    for(int i=0; i<2; i++){
-      if(current < previousSound + (unsigned long)beats[i]*500){
-        if(!playing){
-          tone(speakerPin, tones[i]);  
-          playing = true;
-        }
-      }
-      else playing = false;   
-    }
-  }
-  else if(!playing) noTone(speakerPin);
-
-  previousSound = current;*/
-
   if(debug){
     Serial.print("FSR sensor = ");
     Serial.print(fsrValue);
@@ -71,7 +40,6 @@ void loop() {
     Serial.print("\t flash output = ");
     Serial.println(flashValue);
   }
-
+  
   delay(2);
-
 }
