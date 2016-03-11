eb5d131112e37c786ed0305f01911e7de65d20d1
Yara Grassi Gouffon (RIT Student) <yxg5358@ad.rit.edu>
2016-03-11 09:40:05 -0500

    Starting Project Runway - the Singing Glove. I was gonna map the notes directly to the height of your hand, but the accelerometer fluctuation actually makes such a neat melody I might actually just keep that as  a "random" song that is influenced by your movements.

---
+++ Glove/Glove.ino
@@ -0,0 +1,35 @@
+const int xPin = A0;
+const int yPin = A1;
+const int zPin = A2;
+const int speakerPin = 3;
+const int minAcc = 0;
+const int maxAcc = 10;
+const int minFreq = 261;
+const int maxFreq = 523;
+int xInit;
+int yInit;
+int zInit;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //xInit = analogRead(xPin);
+  yInit = analogRead(yPin);
+  //zInit = analogRead(zPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //int x = analogRead(xPin);
+  int y = analogRead(yPin) - yInit;
+  //int z = analogRead(zPin);
+  
+  if(y < minAcc) y = minAcc;
+  else if(y > maxAcc) y = maxAcc;
+  int sound = map(y, minAcc, maxAcc, minFreq, maxFreq);
+  //Serial.println("y: %d, sound: %d", y, sound);
+  Serial.println(sound);
+  tone(3, sound);
+  delay(100);
+}
