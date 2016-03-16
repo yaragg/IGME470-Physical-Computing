74c1878254cf1eef8d6a9cef23893f5f21cb6199
Yara Grassi Gouffon (RIT Student) <yxg5358@ad.rit.edu>
2016-03-16 09:51:04 -0400

    Testing to see if using a distance makes for more interesting control.

--- Glove/Glove.ino
+++ Glove/Glove.ino
@@ -1,3 +1,4 @@
+
 const int xPin = A0;
 const int yPin = A1;
 const int zPin = A2;
@@ -13,23 +14,26 @@ int zInit;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //xInit = analogRead(xPin);
+  xInit = analogRead(xPin);
   yInit = analogRead(yPin);
-  //zInit = analogRead(zPin);
+  Serial.println(yInit);
+  zInit = analogRead(zPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //int x = analogRead(xPin);
+  int x = analogRead(xPin) - xInit;
   int y = analogRead(yPin) - yInit;
-  //int z = analogRead(zPin);
+  int z = analogRead(zPin) - zInit;
+  
+  y = sqrt(x*x + y*y + z*z);
   
   if(y < minAcc) y = minAcc;
   else if(y > maxAcc) y = maxAcc;
   int sound = map(y, minAcc, maxAcc, minFreq, maxFreq);
   //Serial.println("y: %d, sound: %d", y, sound);
-  Serial.println(sound);
+  //Serial.println(sound);
   tone(3, sound);
   delay(100);
 }
