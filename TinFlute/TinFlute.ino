73e48822b1a7816d31f242b3ae2dc1fc1e9ac9cd
Yara Grassi Gouffon <yara.gouffon@usp.br>
2016-04-18 22:53:56 -0400

    Finished tin whistle

--- TinFlute/TinFlute.ino
+++ TinFlute/TinFlute.ino
@@ -1,4 +1,6 @@
 #define SPEAKER 12
+#define MIC A0
+#define LED 13
 #define F1 2
 #define F2 3
 #define F3 4
@@ -16,6 +18,11 @@
 
 
 int bf1, bf2, bf3, bf4, bf5, bf6;
+int micReading = 0;
+int micMax = 0;
+int micMin = 1023;
+int threshold = 0;
+int difference;
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,12 +32,22 @@ void setup() {
   pinMode(F4, INPUT_PULLUP);
   pinMode(F5, INPUT_PULLUP);
   pinMode(F6, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
+  //Callibrate microphone
+  digitalWrite(LED, HIGH);
+  Serial.begin(9600);
+  while(millis() < 3000){
+    threshold = analogRead(MIC);
+    if(threshold > micMax) micMax = threshold;
+  }
+  digitalWrite(LED, LOW);
+  threshold = micMax;
 
 }
 
 void changeTone(float note)
 {
-  //noTone(SPEAKER);
   tone(SPEAKER, note);
 }
 
@@ -43,14 +60,27 @@ void loop() {
   bf5 = digitalRead(F5);
   bf6 = digitalRead(F6);
 
-  if(!bf1 && !bf2 && !bf3 && !bf4 && !bf5 && !bf6) changeTone(NOTE_D4);
-  else if(!bf1 && !bf2 && !bf3 && !bf4 && !bf5) changeTone(NOTE_E4);
-  else if(!bf1 && !bf2 && !bf3 && !bf4) changeTone(NOTE_F4);
-  else if(!bf1 && !bf2 && !bf3) changeTone(NOTE_G4);
-  else if(!bf1 && !bf2) changeTone(NOTE_A4);
-  else if(!bf1) changeTone(NOTE_B4);
-  else changeTone(NOTE_C5);
+  micReading = analogRead(MIC);
+  micMax = max(micMax, micReading);
+  difference = 0;
+  if(micReading >= threshold){
+    difference = micReading - threshold;
+    Serial.print(difference);
+  }
+  else{
+    noTone(SPEAKER);
+    return;
+  }
 
+  //Play notes. This is based on a simple tin whistle, like this: http://www.irish-folk-songs.com/uploads/4/3/3/6/43368469/2665650.gif?394
+  if(!bf1 && !bf2 && !bf3 && !bf4 && !bf5 && !bf6) changeTone(NOTE_D4);
+  if(!bf1 && !bf2 && !bf3 && !bf4 && !bf5 && bf6) changeTone(NOTE_E4);
+  if(!bf1 && !bf2 && !bf3 && !bf4 && bf5 && bf6) changeTone(NOTE_F4);
+  if(!bf1 && !bf2 && !bf3 && bf4 && bf5 && bf6) changeTone(NOTE_G4);
+  if(!bf1 && !bf2 && bf3 && bf4 && bf5 && bf6) changeTone(NOTE_A4);
+  if(!bf1 && bf2 && bf3 && bf4 && bf5 && bf6) changeTone(NOTE_B4);
+  if(bf1 && bf2 && bf3 && bf4 && bf5 && bf6) changeTone(NOTE_C5);
+  
   delay(2);
 
 }
